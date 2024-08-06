17fe36c2d9bb215190d12d0ff979f9eeab69534f
Jonghyeon Choi <whd617@naver.com>
2024-08-06 15:24:06 +0900

    chore/오리엔테이션

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Circle from './Circle';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div>
-      <Circle borderColor='yellow' bgColor='teal' />
-      <Circle text='im here' bgColor='tomato' />
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
