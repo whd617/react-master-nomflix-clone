f1706cc5bdd8bfe636fd126cc1661de7af476033
Jonghyeon Choi <whd617@naver.com>
2024-08-09 11:32:51 +0900

    chore/react query 적용법

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const queryClient = new QueryClient();
+
 root.render(
   <>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </>,
 );
