7dce285693d831f716e391595fd9c7ba626cf283
whd617 <whd617@naver.com>
2024-09-19 22:05:24 +0900

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,26 @@
 import React from 'react';
-import { DragDropContext, DropResult } from '@hello-pangea/dnd';
-import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
-import { toDoState } from './atoms';
-import Board from './Components/Board';
 
 const Wrapper = styled.div`
-  display: flex;
   max-width: 680px;
   width: 100%;
-  margin: 0 auto;
   justify-content: center;
   align-items: center;
-  height: 100vh;
 `;
 
-const Boards = styled.div`
-  display: grid;
-  width: 100%;
-  gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = (info: DropResult) => {
-    const { destination, draggableId, source } = info;
-    if (!destination) return;
-    if (destination.droppableId === source.droppableId) {
-      // same board movement
-      setToDos((oldToDos) => {
-        // source의 droppableId로 부터 array를 복사하는 과정이야.
-        const boardCopy = [...oldToDos[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        // 1) Delete item on source.index
-        boardCopy.splice(source.index, 1);
-        // 2) Put back the item on the destination.index
-        boardCopy.splice(destination?.index, 0, taskObj);
-        return {
-          ...oldToDos,
-          [source.droppableId]: boardCopy,
-        };
-      });
-    }
-    if (destination.droppableId !== source.droppableId) {
-      // cross board movement
-      setToDos((allBoards) => {
-        // 이동이 시작된 지점의 board Id를 알 수 있다.
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        // destinaton.droppableId는 움직임이 끝나느 board의 ID를 준다.
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map((boardId) => (
-            <Board boardId={boardId} toDos={toDos[boardId]} key={boardId} />
-          ))}
-        </Boards>
-      </Wrapper>
-    </DragDropContext>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 
