f7ec7392b59e7c87bad98f2c470e5173cee8426f
whd617 <whd617@naver.com>
2024-09-21 17:42:26 +0900

    feat/gesture를 활용한 drag event 애니메이션 구현(심화)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { motion, Variants } from 'framer-motion';
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -19,21 +29,26 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants: Variants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: '100px' },
-  drag: { backgroundColor: 'rgb(46,204,113)', transition: { duration: 10 } },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: '100px' },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover='hover'
-        whileDrag='drag'
-        whileTap='click'
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragConstraints={biggerBoxRef}
+          dragElastic={0.5}
+          variants={boxVariants}
+          whileHover='hover'
+          whileDrag='drag'
+          whileTap='click'
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
